5abcb727d1f7bba445691959b4739a4881c05044
Bhaskarown <bhaskarkumar10@gmail.com>
2019-09-18 16:51:47 +0200

    commit on 18Sep at 16:51

---
+++ src/main/java/maven/test/TestMaven.java
@@ -0,0 +1,40 @@
+package maven.test;
+
+public class TestMaven {
+
+	
+	
+		
+		// int x;
+
+	TestMaven () {
+			
+			this(0);
+			System.out.println("Hi Hi");
+			
+		}
+		
+	TestMaven (int x) {
+			
+			System.out.println("How are you");
+			
+			
+		}
+		
+		
+		
+		
+		
+	public static void main (String arg[]) {
+
+		TestMaven obj = new TestMaven();
+	
+	
+	}
+	
+	
+	
+	
+
+
+}
